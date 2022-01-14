cb330f203ef1bf7337b59f7545e361cfda47d82b
ZyXianzi <ichjliebendich@163.com>
2022-01-14 23:18:06 +0800

    init game website

--- game/views.py
+++ game/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">My first Website !</h1>'
+    return HttpResponse(line1)
