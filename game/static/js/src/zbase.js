41da4abce43798873b308e7c8344a6f16a44aed1
ZyXianzi <ichjliebendich@163.com>
2022-01-18 20:56:50 +0800

    add settings_getinfo

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,8 +1,10 @@
 // 总脚本
 export class WarlockGame {
-    constructor(id) {
+    constructor(id, AcWingOS) {
         this.id = id;
         this.$warlock_game = $('#' + id);
+        this.AcWingOS = AcWingOS;
+        
         this.menu = new WarlockGameMenu(this);
         this.playground = new WarlockGamePlayground(this);
         // this.settings = new WarlockGameSettings(this);
