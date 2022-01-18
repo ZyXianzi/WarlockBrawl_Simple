e7cf55e3a0c077023d1e5546af715a133497eb54
ZyXianzi <ichjliebendich@163.com>
2022-01-18 23:07:35 +0800

    add login page

--- static/js/src/settings/zbase.js
+++ static/js/src/settings/zbase.js
@@ -6,6 +6,54 @@ class Settings {
         this.username = "";
         this.photo = "";
 
+        this.$settings = $(`
+<div class="warlock_game_settings">
+    <div class="warlock_game_settings_login">
+        <div class="warlock_game_settings_title">
+            登录
+        </div>
+        <div class="warlock_game_settings_username">
+            <div class="warlock_game_settings_item">
+                <input type="text" placeholder="用户名">
+            </div>
+        </div>
+        <div class="warlock_game_settings_password">
+            <div class="warlock_game_settings_item">
+                <input type="password" placeholder="密码">
+            </div>
+        </div>
+        <div class="warlock_game_settings_submit">
+            <div class="warlock_game_settings_item">
+                <button>登录</button>
+            </div>
+        </div>
+        <div class="warlock_game_settings_error_messages">
+        </div>
+        <div class="warlock_game_settings_option">
+            注册
+        </div>
+        <br>
+        <div class="warlock_game_settings_acwing">
+            <img width="30" src="https://app1186.acapp.acwing.com.cn/static/image/settings/acwing_logo.png">
+            <br>
+            <div>
+                AcWing一键登录
+            </div>
+        </div>
+    </div>
+    <div class="warlock_game_settings_register">
+
+    </div>
+</div>
+`);
+        this.$login = this.$settings.find(".warlock_game_settings_login");
+        this.$login.hide();
+
+        this.$register = this.$settings.find(".warlock_game_settings_register");
+        this.$register.hide();
+
+        this.root.$warlock_game.append(this.$settings);
+
         this.start();
     }
 
@@ -14,11 +62,13 @@ class Settings {
     }
 
     register() {  // 打开注册界面
-
+        this.$login.hide();
+        this.$register.show();
     }
 
     login() {  // 打开登录界面
-
+        this.$register.hide();
+        this.$login.show();
     }
 
     getinfo() {
@@ -46,10 +96,10 @@ class Settings {
     }
 
     show() {  // 打开settings界面
-       
+        this.$settings.show();
     }
 
     hide() {  // 关闭settings界面
-        
+        this.$settings.hide();
     }
 }
